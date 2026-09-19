b0234ecefccf82612e2c15f04ea99325a465cc58
Shreya Patel <shreyapatel3269@gmail.com>
2026-09-19 19:29:38 +0530

    Recommit changes after soft reset

--- app.py
+++ app.py
@@ -1,7 +1,16 @@
-from flask import Flask, jsonify, render_template
+from flask import Flask, jsonify, render_template, request
 import json
-
+import os
+from pymongo import MongoClient
+from dotenv import load_dotenv
 app = Flask(__name__)
+load_dotenv()
+
+mongo_uri = os.getenv("MONGO_URI")
+client = MongoClient(mongo_uri)
+
+db = client["git_github_db"]
+collection = db["todos"]
 
 
 @app.route("/")
@@ -15,6 +24,17 @@ def api():
         data = json.load(file)
 
     return jsonify(data)
+@app.route('/submittodoitem', methods=['POST'])
+def submit_todo_item():
+    item_name = request.form.get('itemName', '').strip()
+    item_description = request.form.get('itemDescription', '').strip()
+
+    collection.insert_one({
+        'itemName': item_name,
+        'itemDescription': item_description
+    })
+
+    return "To-Do item submitted successfully"
 
 
 if __name__ == "__main__":
