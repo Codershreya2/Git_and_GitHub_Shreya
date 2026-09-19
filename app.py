e6151f44625664a9a216dc719d01296c162bf61a
Shreya Patel <shreyapatel3269@gmail.com>
2026-09-19 18:12:46 +0530

    Add initial Flask project

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, jsonify, render_template
+import json
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def home():
+    return render_template("index.html")
+
+
+@app.route("/api")
+def api():
+    with open("data.json", "r") as file:
+        data = json.load(file)
+
+    return jsonify(data)
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
